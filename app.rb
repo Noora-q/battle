f00d58f6f2fda3f0c8fd59c5d7f5e5846634e037
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-03-03 10:07:37 +0000

    Updated filenames

--- app.rb
+++ app.rb
@@ -3,15 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   get '/' do
     'Hello Battle!'
-    "player1 = " << session[:player1].inspect
-    "player2 = " << session[:player2].inspect
   end
   run if app_file == $0
 
   enable :sessions
 
-  get '/battle/add_players' do
-    erb(:add_names)
+  get '/names' do
+    erb(:index)
   end
 
   # get '/:player1' do
@@ -22,17 +20,17 @@ class Battle < Sinatra::Base
   #   session['player2'] = params['player2']
   # end
 
-  post '/battle/set_players' do
+  post '/names' do
     p params
     session['player1'] = params[:player1]
     session['player2'] = params[:player2]
-    redirect '/battle/show_players'
+    redirect '/play'
   end
 
-  get '/battle/show_players' do
+  get '/play' do
     @player1 = session['player1']
     @player2 = session['player2']
     @player2hp = "65"
-    erb(:show_names)
+    erb(:play)
   end
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -6,11 +6,8 @@ require 'spec_helper'
 # Rspec.feature "User Stories", :type => :feature do
 RSpec.feature 'initializing battle game with 2 players' do
   scenario 'allows players to enter their names and see them' do
-    visit "/battle/add_players"
-    fill_in "player1", :with => "Noora"
-    fill_in "player2", :with => "Alex"
-    click_button "Submit"
-    expect(page).to have_text("Player 1: Noora, Player 2: Alex")
+    sign_in_and_play
+    expect(page).to have_text("Noora" && "Alex")
   end
 end
   # As Player 1,
@@ -18,10 +15,7 @@ end
   # I want to see Player 2's Hit Points
   RSpec.feature 'recording hit points' do
     scenario 'shows player 2\'s hit points' do
-      visit "/battle/add_players"
-      fill_in "player1", :with => "Noora"
-      fill_in "player2", :with => "Alex"
-      click_button "Submit"
+      sign_in_and_play
       expect(page).to have_css("progress[value='65']")
     end
 end
